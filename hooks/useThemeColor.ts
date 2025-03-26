69fc748ce5a9472b3fbc4ba7027e5024ff4a9a58
dloyd09 <38634715+dloyd09@users.noreply.github.com>
2025-03-26 10:05:12 -0400

    init commit of the working app

---
+++ hooks/useThemeColor.ts
@@ -0,0 +1,40 @@
+/**
+ * Learn more about light and dark modes:
+ * https://docs.expo.dev/guides/color-schemes/
+ */
+
+import { useColorScheme } from 'react-native';
+
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export function useThemeColor(
+  props: { light?: string; dark?: string },
+  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+) {
+  const theme = useColorScheme() ?? 'light';
+  const colorFromProps = props[theme];
+
+  if (colorFromProps) {
+    return colorFromProps;
+  }
+
+  return Colors[theme][colorName];
+}
+
+const Colors = {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
