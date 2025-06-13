2ac3b4f2c73c13a3d7a4a905084bbba6c39808b9
dloyd09 <38634715+dloyd09@users.noreply.github.com>
2025-06-13 17:22:26 -0400

    removed ios folder

--- ios/AEPSampleAppNewArchEnabled/AdobeModule.m
+++
@@ -1,7 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(AdobeModule, NSObject)
-
-RCT_EXTERN_METHOD(setAppId:(NSString *)appId)
-
-@end 
--- ios/AEPSampleAppNewArchEnabled/AppIdModule.m
+++
@@ -1,7 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(AppIdModule, NSObject)
-
-RCT_EXTERN_METHOD(configureWithAppId:(NSString *)appId)
-
-@end 
